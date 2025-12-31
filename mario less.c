f91ce2ae49aa76a00f419439dc07f0cc2226b406
103425104-cmd <103425104@naval-acad.bg>
2025-12-31 22:50:39 +0200

    Create mario less.c

---
+++ mario less.c
@@ -0,0 +1,15 @@
+def main():
+    while True:
+        try:
+            n = int(input("Height: "))
+            if 1 <= n <= 8:
+                break
+        except ValueError:
+            continue
+
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "#" * i)
+
+
+if __name__ == "__main__":
+    main()
